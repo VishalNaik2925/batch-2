64c5e5021a8c243173b33d5734ec77a328387825
VishalNaik2925 <vishalnaikbarkur2004@gmail.com>
2025-04-19 23:44:18 +0530

    first commit

---
+++ color-generator/src/App.jsx
@@ -0,0 +1,12 @@
+import ColorGenerator from './ColorGenerator'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ColorGenerator />
+    </div>
+  )
+}
+
+export default App
